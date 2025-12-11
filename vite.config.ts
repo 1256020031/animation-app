9a2ff1bb27205ef229d334c77cb71d3e657f3d4d
liuj <1256020031@.qq.com>
2025-12-11 17:47:24 +0800

    fix：后端代理

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  },
   plugins: [
     vue(),
     Components({
